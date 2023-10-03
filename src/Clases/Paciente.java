b16eabea5b466f82e6fd0b97451118c73ee67c1c
Nahuel Fuentes <NF288010@fi365.ort.edu.uy>
2023-10-02 21:59:07 -0300

    Se hizo la gran mayoría del proyecto.

---
+++ src/Clases/Paciente.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author nfuen
+ */
+public class Paciente implements Comparable<Paciente>{
+    
+    private String nombre;
+    
+    private int CI;
+    
+    private String direccion;
+    
+    public Paciente(String elNombre, int laCI, String laDireccion){
+        this.nombre = elNombre;
+        this.CI = laCI;
+        this.direccion = laDireccion;
+    }
+
+    public Paciente() {
+        
+    }
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the CI
+     */
+    public int getCI() {
+        return CI;
+    }
+
+    /**
+     * @param CI the CI to set
+     */
+    public void setCI(int CI) {
+        this.CI = CI;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public int compareTo(Paciente o) {
+        return this.getNombre().compareTo(o.getNombre());
+    }
+    @Override
+    public boolean equals(Object o){
+        Paciente p = (Paciente) o;
+        return this.getCI()== p.getCI();
+    }
+    
+    @Override
+    public String toString() {
+        return this.getNombre();
+    }
+}
